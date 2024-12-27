e357f0caca32567f1ec87be246b61e0755aa5a31
Zeresenayaregal <zeresenayyaregal4@gmail.com>
2024-12-27 21:12:17 +0300

    components created

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import React from "react"
+import waiter from "./images/waiter.jpg"
+import empty_star from "./images/empty_star.png"
+import filled_star from "./images/filled_star.png"
+import "./index.css"
+
+export default function App() {
+    const [contact, setContact] = React.useState({
+        firstName: "John",
+        lastName: "Doe",
+        phone: "+1 (212) 555-1212",
+        email: "itsmyrealname@example.com",
+        isFavorite: false
+    })
+    
+    let starIcon = contact.isFavorite ? filled_star : empty_star
+
+    function toggleFavorite() {
+        setContact(prevContact => {
+            return {
+                ...prevContact,
+                isFavorite: !prevContact.isFavorite
+            }
+        })
+    }
+
+    return (
+        <main>
+            <article className="card">
+                <img
+                    src={waiter}
+                    className="avatar"
+                    alt="User profile picture of John Doe"
+                />
+                <div className="info">
+                    <button
+                        onClick={toggleFavorite}
+                        aria-pressed={contact.isFavorite}
+                        aria-label={contact.isFavorite ? "Added to favorites":"Not added to favorites"}
+                        className="favorite-button"
+                    >
+                        <img
+                            src={starIcon}
+                            alt={contact.isFavorite ? "star filled" : "empty star icon"}
+                            className="favorite"
+                        />
+                    </button>
+                    <h2 className="name">
+                        {contact.firstName} {contact.lastName}
+                    </h2>
+                    <p className="contact">{contact.phone}</p>
+                    <p className="contact">{contact.email}</p>
+                </div>
+
+            </article>
+        </main>
+    )
+}
